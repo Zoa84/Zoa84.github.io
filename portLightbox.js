edaa0063e9c4e20abcf3aa18df0319ae48c5944c
Zoa84 <simonchiu@hotmail.co.uk>
2019-09-01 17:51:05 +0100

    Added comments for portLightBox

--- portLightbox.js
+++ portLightbox.js
@@ -1,29 +1,31 @@
+//Enums for the different portfolio image gallerys
 var ImgEnum = {
     aiImg: 101,
     leapImg: 102,
     lickImg: 103,
 }
 
-var iNextImgTime = 3000;
+var iNextImgTime = 3000;            //Time until the next image (milliseconds)
 
-var aiImgs = [];
-var aiCurrentImg = 0;
-aiImgs[0] = "./images/ai1.png";
+var aiImgs = [];                    //Array of all images for Annihilation Intelligence
+var aiCurrentImg = 0;               //The currently shown image
+aiImgs[0] = "./images/ai1.png";     //The image locations
 aiImgs[1] = "./images/ai2.png";
 aiImgs[2] = "./images/ai3.png";
 
-var leapImgs = [];
+var leapImgs = [];                  //Array of all images for Leap Engine
 var leapCurrentImg = 0;
 leapImgs[0] = "./images/leap1.png";
 leapImgs[1] = "./images/leap2.png";
 leapImgs[2] = "./images/leap3.png";
 
-var lickImgs = [];
+var lickImgs = [];                  //Array of all images for Lick's Cavern
 var lickCurrentImg = 0;
 lickImgs[0] = "./images/lick1.jpg";
 lickImgs[1] = "./images/lick2.jpg";
 lickImgs[2] = "./images/lick3.jpg";
 
+//Show the next image for all portfolio image gallerys
 function NextImage() {
     aiCurrentImg++;
     if (aiCurrentImg > aiImgs.length - 1) {
@@ -44,6 +46,7 @@ function NextImage() {
     document.getElementById("lickIMG").src = lickImgs[lickCurrentImg];
 }
 
+//Start cycling for all image gallerys
 function StartCycleImgs() {
     setInterval(NextImage, iNextImgTime);
 }
